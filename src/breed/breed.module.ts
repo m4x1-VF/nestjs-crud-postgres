2b9b37b1727a051e890db8c5ed6a299764a6d07c
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2023-11-26 12:37:49 +0100

    Fix JWT secret and port configuration

--- src/breed/breed.module.ts
+++ src/breed/breed.module.ts
@@ -3,9 +3,10 @@ import { BreedService } from './breed.service';
 import { BreedController } from './breed.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Breed } from './entities/breed.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Breed])],
+  imports: [TypeOrmModule.forFeature([Breed]), AuthModule],
   controllers: [BreedController],
   providers: [BreedService],
   exports: [TypeOrmModule],
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cat } from './entities/cat.entity';
-import { BreedModule } from 'src/breed/breed.module';
-import { BreedService } from 'src/breed/breed.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BreedModule } from '../breed/breed.module';
+import { BreedService } from '../breed/breed.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cat]), BreedModule],
+  imports: [TypeOrmModule.forFeature([Cat]), BreedModule, AuthModule],
   controllers: [CatsController],
   providers: [CatsService, BreedService],
 })
